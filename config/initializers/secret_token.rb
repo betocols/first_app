857665a6eaf285a5b631d9f23a5171fee8d9f668
betocols <betocolsf@gmail.com>
2014-05-16 14:11:23 -0430

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '5de7daaf76fde2ba80f2c01b10f109c3a11973536eeee3f63f973567ff1537b491d7e081719bdc4da06992ad94d93cd626df34485a437d43e1a76823f5a1371c'
